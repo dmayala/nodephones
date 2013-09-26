347e1b3db5cdc06d55b6150225dee63f7ee9c61b
Daniel Ayala <dmayala@csupomona.edu>
2013-09-26 09:21:40 -0700

    Added handlebars template for Coinbase buttons.

--- client/js/views/cbbutton.js
+++ client/js/views/cbbutton.js
@@ -1,7 +1,7 @@
-define(['backbone'], function(Backbone) {
+define(['backbone', 'hbs!templates/cbButtonTpl'], function(Backbone, CbButtonTpl) {
   var CbButtonView = Backbone.View.extend({
 
-    template: _.template('<a class="coinbase-button" data-code="<%= code %>" data-button-style="custom_large"></a><script type="text/javascript">$(document).ready(function() {$(".my-custom-link").hide(); $.getScript("https://coinbase.com/assets/button.js", function() { $("iframe").load(function() { $(".my-custom-link").show(); $(".my-custom-link").click(function(){$(document).trigger("coinbase_show_modal", "<%= code %>");return false;});$(document).on("coinbase_payment_complete", function(event, code){console.log("Payment completed for button "+code);window.location = "/confirmation.html";});}); });});</script>'),
+    template: CbButtonTpl,
 
     //listen to models
     initialize: function() {
